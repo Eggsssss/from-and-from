a68541052af1cd3af0e9194d64c3bd9f1204b1f5
Zander <97479601+Eggsssss@users.noreply.github.com>
2024-01-10 10:38:38 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// from: In from method you don't have to specify the inital state, (It will figure out by itself)
+
+// gsap.method(element, vars-obj)
+
+
+
+// gsap.from(".box1", {
+   // y: -200,
+   // duration: 3,
+   // ease: "linear",
+   // repeat: -1,
+   // yoyo: true,
+// });
+
+// fromTo: In fromTo method you HAVE to specify the default value, and also the new animation state.
+
+gsap.fromTo(
+    ".box2",
+{
+    y: 200, 
+    opacity: 0,
+}, 
+{
+    opacity: 1,
+    y: -200,
+    duration: 3,
+    ease: "linear",
+    borderRadius: 0,
+    repeat: -1,
+    yoyo: true,
+})
+
